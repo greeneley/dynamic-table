57118a860abfb44b47f3f682a14fc6e8cd254465
greeneley <anhchangtoanhoc97@gmail.com>
2022-06-05 22:42:12 +0700

    add new items

---
+++ src/App.js
@@ -0,0 +1,84 @@
+// import logo from './logo.svg';
+import "./App.css";
+
+import React from "react";
+
+export default class DynamicTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      items: [],
+    };
+  }
+
+  updateMessage(event) {
+    this.setState({
+      message: event.target.value,
+    });
+  }
+
+  handleClick() {
+    var items = this.state.items;
+    items.push(this.state.message);
+    this.setState({
+      items: items,
+    });
+  }
+
+  handleItemChanged(i, event) {
+    var items = this.state.items;
+    items[i] = event.target.value;
+
+    this.setState({
+      items: items,
+    });
+  }
+
+  handleItemDelete(i) {
+    var items = this.state.items;
+    items.splice(i, 1);
+
+    this.setState({
+      items: items
+    });
+  }
+  renderRows() {
+    var context = this;
+    return this.state.items.map(function (o, i) {
+      return (
+        <tr key={"item-" + i}>
+          <td>
+            <input
+              type="text"
+              value={o}
+              onChange={context.handleItemChanged.bind(i, context)}
+            />
+          </td>
+          <td>
+            <button onClick={context.handleItemDelete.bind(context, i)}>Delete</button>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderRows()}</tbody>
+        </table>
+        <hr />
+        <input type="text" onChange={this.updateMessage.bind(this)} />
+        <button onClick={this.handleClick.bind(this)}>Add Item</button>
+      </div>
+    );
+  }
+}
